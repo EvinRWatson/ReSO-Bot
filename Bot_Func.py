239ec8794ad6f4b6cf0c5ef2630e32e470f25bdb
Evin Watson <evinwatson@outlook.com>
2022-10-20 18:32:12 -0400

    Reorganized functions in file

--- Bot_Func.py
+++ Bot_Func.py
@@ -17,27 +17,18 @@ def prevent_command_chaining(input: str):
         raise PermissionError("Invalid Characters")
 
 
+def prevent_start_without_token(config):
+    while str(config['general']['botToken']) == "":
+        print("Bot Token Empty. Restart bot after configuration")
+        time.sleep(60)
+
+
 def get_object_by_name(name, objects):
     for obj in objects:
         if name == obj['name']:
             return obj
 
     raise KeyError(f'{name} Not Found')
-
-
-def initialize_logger():
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='a')
-    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-    logger.addHandler(handler)
-    return logger
-
-
-def prevent_start_without_token(config):
-    while str(config['general']['botToken']) == "":
-        print("Bot Token Empty. Restart bot after configuration")
-        time.sleep(60)
 
 
 def get_help_message(config):
@@ -52,6 +43,15 @@ def get_help_message(config):
         output += f"\t{script['name']}\n"
 
     return output
+
+
+def initialize_logger():
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(filename='bot.log', encoding='utf-8', mode='a')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+    return logger
 
 
 def log_action(message: str, logger: logging.Logger, ctx: interactions.CommandContext = None):
